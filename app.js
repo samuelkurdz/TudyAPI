216c3d37e9adc8672fecdfef6d5db7e9e188ede0
sambayo <samuel@pledre.com>
2020-04-02 20:18:45 +0100

    CORS enabled

--- app.js
+++ app.js
@@ -13,13 +13,19 @@ const { List, Task } = require('./app_api/models/fullSchema');
 
 const apiRouter = require('./app_api/routes/index');
 
+//MiddleWares
 const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'app_server', 'views'));
-// app.set('view engine', 'jade');
-
 app.use(bodyParser.json());
+
+
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
+  next();
+})
+
 
 app.use(logger('dev'));
 app.use(express.json());
